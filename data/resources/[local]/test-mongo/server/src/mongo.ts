904fe1fb1558dd22ebc7b5761d46a6cdf5ecb5ba
Vincent Andrieu <vincent.andrieu@epitech.eu>
2024-09-29 16:03:58 +0200

    Edit use convar variables

--- data/resources/[local]/test-mongo/server/src/mongo.ts
+++ data/resources/[local]/test-mongo/server/src/mongo.ts
@@ -1,14 +1,6 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import mongoose from "mongoose";
 
-const env = {
-    MONGO_URI: undefined,
-    MONGO_HOST: "localhost",
-    MONGO_PORT: "27017",
-    MONGO_DB_NAME: "test",
-    MONGO_USERNAME: undefined,
-    MONGO_PASSWORD: undefined
-};
+const MONGO_URI = GetConvar("MONGO_URI", "mongodb://localhost:27017/test");
 
 mongoose.set("strictQuery", false);
 
@@ -23,29 +15,17 @@ export async function initMongo(): Promise<void> {
     }
     console.info(
         "Mongo successfully connected : \n\t- Address : " +
-            env.MONGO_HOST +
+            mongoose.connection.host +
             "\n\t- Port : " +
-            env.MONGO_PORT +
+            mongoose.connection.port +
             "\n\t- Name : " +
-            env.MONGO_DB_NAME
+            mongoose.connection.name
     );
 }
 
 function getMongoUrl(): string {
-    if (env.MONGO_URI) {
-        return env.MONGO_URI;
+    if (MONGO_URI) {
+        return MONGO_URI;
     }
-    if (!env.MONGO_HOST || !env.MONGO_PORT || !env.MONGO_DB_NAME) {
-        throw new Error("Missing database configuration");
-    }
-    const baseUri = `${env.MONGO_HOST}:${env.MONGO_PORT}/${env.MONGO_DB_NAME}`;
-    const uri =
-        env.MONGO_USERNAME && env.MONGO_PASSWORD
-            ? `mongodb://${env.MONGO_USERNAME}:${env.MONGO_PASSWORD}@${baseUri}?authSource=admin`
-            : `mongodb://${baseUri}`;
-
-    if ((env.MONGO_USERNAME && !env.MONGO_PASSWORD) || (!env.MONGO_USERNAME && env.MONGO_PASSWORD)) {
-        throw new Error("Invalid database credentials");
-    }
-    return uri;
+    throw new Error("Missing database configuration");
 }
