5ea144130854ce0bb57ec6f88446dff18c816cea
Vincent Andrieu <v.andrieu@live.com>
2025-11-26 23:03:51 +0100

    Add redis core

--- data/shared/server/src/index.ts
+++ data/shared/server/src/index.ts
@@ -2,6 +2,7 @@ import "./polyfill";
 
 import PlayersModel from "./models/playersModel";
 import { initMongo } from "./mongo";
+import { initRedis } from "./redis";
 import { isObjectId, ObjectId, toObjectId } from "./utils";
 
-export { initMongo, isObjectId, ObjectId, PlayersModel, toObjectId };
+export { initMongo, initRedis, isObjectId, ObjectId, PlayersModel, toObjectId };
