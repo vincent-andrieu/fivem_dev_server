dae0f1567466f9cb3485c2bf5201fe4adb91214c
Vincent Andrieu <v.andrieu@live.com>
2025-11-27 01:22:55 +0100

    WIP: Remove eslint

--- data/shared/core/src/utils.ts
+++ data/shared/core/src/utils.ts
@@ -1,5 +1,4 @@
 export type NonFunctionPropertyNames<T> = {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
     [K in keyof T]: T[K] extends Function ? never : K;
 }[keyof T];
 export type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
